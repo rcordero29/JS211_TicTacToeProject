956a092502c1a701b46feab9560f7aef9bce6024
Ruben Cordero <ruben.cordero29@yahoo.com>
2021-10-03 14:13:56 -0500

    adds markers but does not say when win happens, very buggy.

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-'use strict';
+// 'use strict';
 
-// brings in the assert module for unit testing
-const assert = require('assert');
-// brings in the readline module to access the command line
-const readline = require('readline');
-// use the readline module to print out to the command line
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+// // brings in the assert module for unit testing
+// const assert = require('assert');
+// // brings in the readline module to access the command line
+// const readline = require('readline');
+// // use the readline module to print out to the command line
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
 
 // creates and empty "board" for the user to see where marks can be placed.
 // using let because the variable is expected to change with more 'X's and 'O's to add
@@ -141,60 +141,60 @@ const getPrompt = () => {
   });
 };
 
-// Unit Tests
-// You use them run the command: npm test main.js
-// to close them ctrl + C
-if (typeof describe === 'function') {
-  describe('#ticTacToe()', () => {
-    it('should place mark on the board', () => {
-      ticTacToe(1, 1);
-      assert.deepEqual(board, [
-        [' ', ' ', ' '],
-        [' ', 'X', ' '],
-        [' ', ' ', ' '],
-      ]);
-    });
-    it('should alternate between players', () => {
-      ticTacToe(0, 0);
-      assert.deepEqual(board, [
-        ['O', ' ', ' '],
-        [' ', 'X', ' '],
-        [' ', ' ', ' '],
-      ]);
-    });
-    it('should check for vertical wins', () => {
-      board = [
-        [' ', 'X', ' '],
-        [' ', 'X', ' '],
-        [' ', 'X', ' '],
-      ];
-      assert.equal(verticalWin(), true);
-    });
-    it('should check for horizontal wins', () => {
-      board = [
-        ['X', 'X', 'X'],
-        [' ', ' ', ' '],
-        [' ', ' ', ' '],
-      ];
-      assert.equal(horizontalWin(), true);
-    });
-    it('should check for diagonal wins', () => {
-      board = [
-        ['X', ' ', ' '],
-        [' ', 'X', ' '],
-        [' ', ' ', 'X'],
-      ];
-      assert.equal(diagonalWin(), true);
-    });
-    it('should detect a win', () => {
-      ticTacToe(0, 0);
-      ticTacToe(0, 1);
-      ticTacToe(1, 1);
-      ticTacToe(0, 2);
-      ticTacToe(2, 2);
-      assert.equal(checkForWin(), true);
-    });
-  });
-} else {
-  getPrompt();
-}
+// // Unit Tests
+// // You use them run the command: npm test main.js
+// // to close them ctrl + C
+// if (typeof describe === 'function') {
+//   describe('#ticTacToe()', () => {
+//     it('should place mark on the board', () => {
+//       ticTacToe(1, 1);
+//       assert.deepEqual(board, [
+//         [' ', ' ', ' '],
+//         [' ', 'X', ' '],
+//         [' ', ' ', ' '],
+//       ]);
+//     });
+//     it('should alternate between players', () => {
+//       ticTacToe(0, 0);
+//       assert.deepEqual(board, [
+//         ['O', ' ', ' '],
+//         [' ', 'X', ' '],
+//         [' ', ' ', ' '],
+//       ]);
+//     });
+//     it('should check for vertical wins', () => {
+//       board = [
+//         [' ', 'X', ' '],
+//         [' ', 'X', ' '],
+//         [' ', 'X', ' '],
+//       ];
+//       assert.equal(verticalWin(), true);
+//     });
+//     it('should check for horizontal wins', () => {
+//       board = [
+//         ['X', 'X', 'X'],
+//         [' ', ' ', ' '],
+//         [' ', ' ', ' '],
+//       ];
+//       assert.equal(horizontalWin(), true);
+//     });
+//     it('should check for diagonal wins', () => {
+//       board = [
+//         ['X', ' ', ' '],
+//         [' ', 'X', ' '],
+//         [' ', ' ', 'X'],
+//       ];
+//       assert.equal(diagonalWin(), true);
+//     });
+//     it('should detect a win', () => {
+//       ticTacToe(0, 0);
+//       ticTacToe(0, 1);
+//       ticTacToe(1, 1);
+//       ticTacToe(0, 2);
+//       ticTacToe(2, 2);
+//       assert.equal(checkForWin(), true);
+//     });
+//   });
+// } else {
+//   getPrompt();
+// }
